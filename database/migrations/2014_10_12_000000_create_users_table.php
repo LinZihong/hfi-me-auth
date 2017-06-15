a7af074abc57cb5e0ad806409548608f7d517431
yhylord <yhylord@gmail.com>
2017-06-15 22:45:23 +0800

    Config User for Passport

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
+            $table->uuid('uuid');
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('phone');
+            $table->string('wechat');
+            $table->string('avatar');
             $table->rememberToken();
             $table->timestamps();
         });
